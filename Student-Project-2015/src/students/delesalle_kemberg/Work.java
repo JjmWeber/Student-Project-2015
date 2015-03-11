ef9b7ac0cebf935d149460cd4f9093f1c762d368
DamienKemberg <damien.kemberg@gmail.com>
2015-03-11 14:42:46 +0100

    Add pretreatment before the alphaTreeBuilder

--- Student-Project-2015/src/students/delesalle_kemberg/Work.java
+++ Student-Project-2015/src/students/delesalle_kemberg/Work.java
@@ -1,10 +1,39 @@
 package students.delesalle_kemberg;
 
+import java.util.ArrayList;
+
+import main.alphaTree.AlphaTreeBuilder;
+import main.alphaTree.AlphaTreeViewer;
+import main.alphaTree.data.AlphaTree;
+import main.alphaTree.descriptor.AlphaTreeNodeDescriptor;
+import main.alphaTree.descriptor.AlphaTreeNodeDescriptorArea;
+import main.alphaTree.descriptor.AlphaTreeNodeDescriptorOmega;
+import fr.unistra.pelican.ByteImage;
+import fr.unistra.pelican.Image;
+import fr.unistra.pelican.algorithms.conversion.RGBAToGray;
+import fr.unistra.pelican.algorithms.io.ImageLoader;
+import fr.unistra.pelican.algorithms.segmentation.ManualThresholding;
+
 public class Work {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+				ByteImage image = (ByteImage)ImageLoader.exec("pelican/samples/foot.png");
+				
+				//Prétraitements
+				ManualThresholding threshold = new ManualThresholding();
+				Image image2 = RGBAToGray.exec(image);
+				Image BinaryImage = threshold.exec(image2,150);
+				ByteImage img = new ByteImage(BinaryImage);
+				
+				
+				ArrayList<Class<? extends AlphaTreeNodeDescriptor>> descriptors = new ArrayList<Class<? extends AlphaTreeNodeDescriptor>> ();
+				descriptors.add(AlphaTreeNodeDescriptorOmega.class);
+				descriptors.add(AlphaTreeNodeDescriptorArea.class);		
+				long t = System.currentTimeMillis();
+				AlphaTree result = AlphaTreeBuilder.exec(img,descriptors);
+				t=System.currentTimeMillis()-t;
+				System.out.println("Alpha-tree creation time : "+t+"ms");
+				AlphaTreeViewer.exec(result);
 	}
 
-}
+	}
