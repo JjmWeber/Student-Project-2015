d479830d8ec1611b8ce0fc3c0f919d546e2a5de2
Geoffrey <geoffreydesanlis@hotmail.fr>
2015-03-27 15:37:07 +0100

    Corrections de la classe Work duent aux modifications

--- Student-Project-2015/src/students/desanlis_pierrel/Work.java
+++ Student-Project-2015/src/students/desanlis_pierrel/Work.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 import main.alphaTree.AlphaTreeBuilder;
 import main.alphaTree.AlphaTreeViewer;
 import main.alphaTree.data.AlphaTree;
+import main.alphaTree.descriptor.AlphaTreeNodeCutDescriptor;
 import main.alphaTree.descriptor.AlphaTreeNodeDescriptor;
 import main.alphaTree.descriptor.AlphaTreeNodeDescriptorArea;
 import main.alphaTree.descriptor.AlphaTreeNodeDescriptorOmega;
+import main.alphaTree.descriptor.AlphaTreeNodeFilterDescriptor;
 import fr.unistra.pelican.ByteImage;
 import fr.unistra.pelican.algorithms.io.ImageLoader;
 import fr.unistra.pelican.gui.FileChooserToolBox;
@@ -18,12 +20,15 @@ public class Work {
 
 	public static void main(String[] args) {
 		ByteImage lena = (ByteImage)ImageLoader.exec(FileChooserToolBox.openOpenFileChooser(null).getAbsolutePath());
-		ArrayList<Class<? extends AlphaTreeNodeDescriptor>> descriptors = new ArrayList<Class<? extends AlphaTreeNodeDescriptor>> ();
-		//descriptors.add(AlphaTreeNodeDescriptorOmega.class);
-		descriptors.add(AlphaTreeNodeDescriptorArea.class);		
-		descriptors.add(AlphaTreeNodeDescriptorOTeinte.class);
+		
+		ArrayList<Class<? extends AlphaTreeNodeCutDescriptor>> cutDescriptors = new ArrayList<Class<? extends AlphaTreeNodeCutDescriptor>> ();
+		cutDescriptors.add(AlphaTreeNodeDescriptorOmega.class);
+		cutDescriptors.add(AlphaTreeNodeDescriptorOTeinte.class);
+		ArrayList<Class<? extends AlphaTreeNodeFilterDescriptor>> filterDescriptors = new ArrayList<Class<? extends AlphaTreeNodeFilterDescriptor>> ();
+		filterDescriptors.add(AlphaTreeNodeDescriptorArea.class);
+		
 		long t = System.currentTimeMillis();
-		AlphaTree result = AlphaTreeBuilder.exec(lena,descriptors);
+		AlphaTree result = AlphaTreeBuilder.exec(lena,cutDescriptors, filterDescriptors);
 		t=System.currentTimeMillis()-t;
 		System.out.println("Alpha-tree creation time : "+t+"ms");
 		AlphaTreeViewer.exec(result);
