a911c64d5ae80c876c06c087843ae974f0920bac
thomas-delesalle <tdelesallesfr.fr>
2015-08-09 16:17:27 +0200

    Amélioration du traitement des pixels orphelins ajout d'une méthode de fusion des superpixels similaires, les paramètres sont délicats à régler...

--- Student-Project-2015/src/superpixels/SLIC.java
+++ Student-Project-2015/src/superpixels/SLIC.java
@@ -3,7 +3,8 @@ package superpixels;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.HashSet;
+import java.util.Iterator;
 import fr.unistra.pelican.Algorithm;
 import fr.unistra.pelican.AlgorithmException;
 import fr.unistra.pelican.DoubleImage;
@@ -11,7 +12,14 @@ import fr.unistra.pelican.Image;
 import fr.unistra.pelican.IntegerImage;
 import fr.unistra.pelican.algorithms.conversion.RGBToXYZ;
 import fr.unistra.pelican.algorithms.conversion.XYZToLAB;
+import fr.unistra.pelican.algorithms.morphology.connected.ConnectedComponentMap;
 import fr.unistra.pelican.algorithms.morphology.vectorial.gradient.MultispectralEuclideanGradient;
+import fr.unistra.pelican.algorithms.segmentation.labels.DrawFrontiersOnImage;
+import fr.unistra.pelican.algorithms.segmentation.labels.FrontiersFromSegmentation;
+import fr.unistra.pelican.algorithms.visualisation.Viewer2D;
+import fr.unistra.pelican.util.connectivityTrees.connectivity.Connectivity3D;
+import fr.unistra.pelican.util.connectivityTrees.connectivity.FlatConnectivity;
+import fr.unistra.pelican.util.connectivityTrees.connectivity.TrivialConnectivity;
 import fr.unistra.pelican.util.morphology.FlatStructuringElement2D;
 
 /**
@@ -28,6 +36,8 @@ public class SLIC extends Algorithm {
 
 	public Image inputImage;
 	public int numberOfSuperpixels;
+	public boolean fuseSuperpixels;
+
 
 	//Between 1 and 40, low value to respect boundary, high value to respect compactness
 	public double m;
@@ -37,8 +47,7 @@ public class SLIC extends Algorithm {
 	public SLIC()
 	{
 		super();
-		super.inputs="inputImage,numberOfSuperpixels";
-		super.options="m";
+		super.inputs="inputImage,numberOfSuperpixels,m,fuseSuperpixels";
 		super.outputs = "superpixels";
 	}
 
@@ -64,6 +73,9 @@ public class SLIC extends Algorithm {
 			{
 				clusters.add(new Cluster(lab.getPixelXYBDouble(x, y, 0),lab.getPixelXYBDouble(x, y, 1),lab.getPixelXYBDouble(x, y, 2),x,y));
 			}
+
+		final ArrayList<int[]> finalSuperpixelsCore = new ArrayList<int[]>(clusters.size());
+
 
 		//Move cluster center to lowest gradient in 3x3 neighbourhood
 		for(Cluster c : clusters)
@@ -146,6 +158,7 @@ public class SLIC extends Algorithm {
 						newClusterPixelCount[labelValue]++;
 					}
 				}
+			finalSuperpixelsCore.clear();
 			for(int i=0;i<newClusterCenter.length;i++)
 			{
 				int newX = Math.round(((float) newClusterCenter[i][0])/newClusterPixelCount[i]);
@@ -157,15 +170,189 @@ public class SLIC extends Algorithm {
 				clusters.get(i).l=lab.getPixelXYBDouble(newX, newY, 0);
 				clusters.get(i).a=lab.getPixelXYBDouble(newX, newY, 1);
 				clusters.get(i).b=lab.getPixelXYBDouble(newX, newY, 2);
+
+				int[] coordinates = new int[3];
+				coordinates[0] = newX;
+				coordinates[1] = newY;
+				finalSuperpixelsCore.add(i,coordinates);
+
 			}			
 			System.out.println("Loop "+loop+" done ! Residual error : "+error);
 		} while (error!=0);
 
 
+		label = enforceConnectivity(label,finalSuperpixelsCore);
+		if(fuseSuperpixels)
+		label = fuse(label,lab,finalSuperpixelsCore,9,5);
+
 		//Segmentation result
 		superpixels = label;
 
 
+
+	}
+
+	/**
+	 * @param label  the IntegerImage where to enforce connectivity
+	 * @param finalSuperpixelsCore the array with coordinates of the superpixels centers
+	 * @return label the IntegerImage with enforced connectivity
+	 */
+	public IntegerImage enforceConnectivity(IntegerImage label,ArrayList<int[]> finalSuperpixelsCore){
+		System.out.println("We now enforce connectivity");
+		//we grab the label of each superpixel core
+		for(int i = 0; i <finalSuperpixelsCore.size();i++ ){
+			finalSuperpixelsCore.get(i)[2] = label.getPixelXYInt(finalSuperpixelsCore.get(i)[0],finalSuperpixelsCore.get(i)[1]);
+		}
+		int xDim = inputImage.xdim;
+		int yDim = inputImage.ydim;
+		int sizeCriteria = ((xDim*yDim/finalSuperpixelsCore.size())/4);
+		System.out.println("size criteria = "+sizeCriteria);
+
+		//we create an IntegerImage called labelsMapImage to store the informations about connexe components
+		Connectivity3D con = new FlatConnectivity(label, TrivialConnectivity.getHeightNeighbourhood());
+		IntegerImage labelsMapImage = ConnectedComponentMap.exec(label,con);
+
+		//------------we adjust the labelsMapImage to suitable labels by tightening them
+		HashSet<Integer> distinctComponents = new HashSet<Integer>();
+		ArrayList<Integer> correspondanceList = new ArrayList<Integer>();
+		for(int y=0;y<yDim;y++)
+			for(int x=0;x<xDim;x++){
+				int componentLabelValue = labelsMapImage.getPixelXYInt(x,y);
+				if(distinctComponents.add(componentLabelValue)){
+					correspondanceList.add(componentLabelValue);
+				}
+			}
+		System.out.println("numbers of connexe components : "+distinctComponents.size());
+		for(int y=0;y<yDim;y++)
+			for(int x=0;x<xDim;x++){
+				for(int i = 0; i<correspondanceList.size(); i++){
+					if(labelsMapImage.getPixelXYInt(x,y) == correspondanceList.get(i)){
+						labelsMapImage.setPixelXYInt(x,y,i);
+					}
+				}
+			}
+		//--------------------------------------------------------------
+		//we create and fill an array of components
+		Component components[] = new Component[distinctComponents.size()];
+		for(int i = 0; i < components.length; i++){
+			components[i] = new Component(i,new ArrayList<Pixel>());
+		}
+		for(int y=0;y<yDim;y++)
+			for(int x=0;x<xDim;x++){
+				components[labelsMapImage.getPixelXYInt(x,y)].add(new Pixel(x,y));
+			}
+
+		//for each small component, we compute its mean coordinates
+		for(int i = 0; i <components.length ; i++){
+			if(components[i].size() < sizeCriteria){
+				double xMean = 0;
+				double yMean = 0;
+				for(int j = 0; j < components[i].size(); j++){
+					xMean+=components[i].getPixel(j).x;
+					yMean+=components[i].getPixel(j).y;
+				}
+				components[i].xMean = xMean/components[i].size();
+				components[i].yMean = yMean/components[i].size();
+			}
+		}
+
+		//we look for the most suitable superPixel to fuse our small components
+		for(int i = 0; i < components.length; i++){//for each component
+			if(components[i].size() < sizeCriteria){//if the component is too small
+				for(int j = 0; j < finalSuperpixelsCore.size(); j++){
+					double xDiff = components[i].xMean-finalSuperpixelsCore.get(j)[0];
+					double yDiff = components[i].yMean-finalSuperpixelsCore.get(j)[1];
+					double d = Math.sqrt(xDiff*xDiff+yDiff*yDiff);
+					if( d < components[i].minimalDistanceToSuperpixelCore){
+						components[i].minimalDistanceToSuperpixelCore = d;
+						components[i].bestXSuperpixel = finalSuperpixelsCore.get(j)[0];
+						components[i].bestYSuperpixel = finalSuperpixelsCore.get(j)[1];
+					}
+				}
+			}
+		}
+
+		//for each small component we now update the labels
+		for(int i = 0; i < components.length; i++){
+			if(components[i].size() < sizeCriteria){
+				for(int j = 0; j < components[i].size(); j++){
+					int currentX = components[i].getPixel(j).x;
+					int currentY = components[i].getPixel(j).y;
+					int superPixelLabel = label.getPixelXYInt(components[i].bestXSuperpixel, components[i].bestYSuperpixel);
+					label.setPixelXYInt(currentX,currentY,superPixelLabel);
+				}
+			}
+		}
+		return label;
+	}
+
+	/**
+	 * @param label  the IntegerImage containing the labels of the image where to fuse superpixels
+	 * @param lab  the Image where to fuse superpixels
+	 * @param finalSuperpixelsCore the array with coordinates of the superpixels centers
+	 * @param sampleSize the size of the small square around the superpixel cores where to compute a mean used for fusions
+	 * @param labDifference the maximum difference in l, a, and b to consider a fusion between two superpixels
+	 * @return label the IntegerImage with fused superpixels
+	 */
+	public IntegerImage fuse(IntegerImage label, Image lab, ArrayList<int[]> finalSuperpixelsCore, int sampleSize, double labDifference){
+		System.out.println("we now (try) fuse the similar superpixels");
+		Pixel[] superpixels = new Pixel[finalSuperpixelsCore.size()];
+		int step = (int) Math.round(Math.sqrt(((double)label.size())/numberOfSuperpixels));
+		int treshold = 2*step;
+		int sampleMidSide = (int) Math.sqrt(sampleSize)/2;
+		
+		//we fill the array of superpixels with a mean of a small square of pixels around the core
+		for(int i = 0; i < superpixels.length; i++){
+			int midX = finalSuperpixelsCore.get(i)[0];
+			int midY = finalSuperpixelsCore.get(i)[1];
+			double l=0;
+			double a=0;
+			double b=0;			
+			for(int y=midY-sampleMidSide;y<=midY+sampleMidSide;y++){
+				for(int x=midX-sampleMidSide;x<=midX+sampleMidSide;x++){
+					l+=lab.getPixelXYBDouble(x,y,0);
+					a+=lab.getPixelXYBDouble(x,y,1);
+					b+=lab.getPixelXYBDouble(x,y,2);
+				}
+			}
+			l = l/sampleSize;
+			a = a/sampleSize;
+			b = b/sampleSize;
+			superpixels[i] = new Pixel(l,a,b,midX,midY);
+			superpixels[i].label = label.getPixelXYInt(midX,midY);
+			superpixels[i].oldLabel = label.getPixelXYInt(midX,midY);
+		}
+
+		//we look for superpixels to fuse
+		for(int i = 0; i < superpixels.length; i++){
+			for(int j = 0; j < superpixels.length; j++){
+				if(superpixels[i].label != superpixels[j].label){
+					double xDiff = superpixels[i].x-superpixels[j].x;
+					double yDiff = superpixels[i].y-superpixels[j].y;
+					double d = Math.sqrt(xDiff*xDiff+yDiff*yDiff);
+					if( d < treshold){
+						double lDiff = Math.abs(superpixels[i].l-superpixels[j].l);
+						double aDiff = Math.abs(superpixels[i].a-superpixels[j].a);
+						double bDiff = Math.abs(superpixels[i].b-superpixels[j].b);
+						if(lDiff < labDifference && aDiff < labDifference && bDiff < labDifference ){
+							superpixels[i].label = superpixels[j].label;
+						}
+					}
+				}
+			}
+		}
+		//we update the image with the fused labels
+		for(int y=0;y<inputImage.ydim;y++){
+			for(int x=0;x<inputImage.xdim;x++){
+				for(int i = 0; i < superpixels.length; i++){
+					if(label.getPixelXYInt(x,y) == superpixels[i].oldLabel){
+						label.setPixelXYInt(x,y,superpixels[i].label);
+					}
+				}
+			}
+		}
+
+		return label;
 
 	}
 
@@ -185,9 +372,9 @@ public class SLIC extends Algorithm {
 	 * @param m compactness parameter
 	 * @return  SLIC superpixels image
 	 */
-	public static IntegerImage exec(Image inputImage, int numberOfSuperpixels, double m)
+	public static IntegerImage exec(Image inputImage, int numberOfSuperpixels, double m,boolean fuseSuperpixels)
 	{
-		return (IntegerImage) new SLIC().process(inputImage, numberOfSuperpixels,m);
+		return (IntegerImage) new SLIC().process(inputImage, numberOfSuperpixels,m,fuseSuperpixels);
 	}
 
 	private class Cluster
@@ -207,5 +394,61 @@ public class SLIC extends Algorithm {
 			this.y=y;
 		}
 	}
+	private class Pixel{
+		public int x;
+		public int y;
+		public double l;
+		public double a;
+		public double b;
+		public int label;
+		public int oldLabel;
+
+		public Pixel(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+
+		public Pixel(double l, double a, double b, int x, int y){
+			this.l = l;
+			this.a = a;
+			this.b = b;
+			this.x = x;
+			this.y = y;
+		}
+
+		public void display(){
+			System.out.println("("+this.x+";"+this.y+")");
+		}
+	}
+
+	private class Component{
+		public int label;
+		public ArrayList<Pixel> pixels;
+		public double xMean;
+		public double yMean;
+		public double minimalDistanceToSuperpixelCore;
+		public int bestXSuperpixel;
+		public int bestYSuperpixel;
+
+
+		public Component(int label, ArrayList<Pixel> pixels){
+			this.label = label;
+			this.pixels = pixels;
+			this.minimalDistanceToSuperpixelCore = Double.MAX_VALUE;
+
+		}
+
+		public void add(Pixel p){
+			pixels.add(p);
+		}
+		public Pixel getPixel(int i){
+			return this.pixels.get(i);
+		}
+
+		public int size(){
+			return this.pixels.size();
+		}
+	}
+
 
 }
--- Student-Project-2015/src/superpixels/SLICLauncher.java
+++ Student-Project-2015/src/superpixels/SLICLauncher.java
@@ -9,61 +9,49 @@ import fr.unistra.pelican.algorithms.visualisation.Viewer2D;
 public class SLICLauncher {
 
 	public static void main(String[] args) {
-		System.out.println("SLIC and SLICO tests starting...");
+		System.out.println("SLICs tests starting...");
 
 		//Image input = ImageLoader.exec("/home/weber/Documents/git/pelican/samples/curious.png");
 		Image input = ImageLoader.exec("C:/Users/Thomas/git/pelican/samples/curious.png");
-		int numberOfSuperPixels = 500;
+		int numberOfSuperPixels = 800;
 		int m = 20;
-
-
-
+		boolean fuseSuperpixels = false;
+		double start;
+		double end;
 
 		//SLIC (m parameter to tune)
-/*		double start = System.currentTimeMillis();
-		Image outputSLIC = SLIC.exec(input, numberOfSuperPixels,m);
-		double end = System.currentTimeMillis();
+		start = System.currentTimeMillis();
+		Image outputSLIC = SLIC.exec(input, numberOfSuperPixels,m,fuseSuperpixels);
+		end = System.currentTimeMillis();
 		double executionTimeSLIC = end - start;
 		Viewer2D.exec(DrawFrontiersOnImage.exec(input, FrontiersFromSegmentation.exec(outputSLIC)),"SLIC : "+numberOfSuperPixels+" superpixels, m = "+m);
-*/
-		
-		
-		//SLICOrphanPixels (m parameter to tune)
-		double start = System.currentTimeMillis();
-		Image outputSLICOrphanPixels = SLICOrphanPixels.exec(input, numberOfSuperPixels,m);
-		double end = System.currentTimeMillis();
-		double executionTimeSLICOrphanPixels = end - start;
-		Viewer2D.exec(DrawFrontiersOnImage.exec(input, FrontiersFromSegmentation.exec(outputSLICOrphanPixels)),"SLICOrphanPixels : "+numberOfSuperPixels+" superpixels, m = "+m);
 
 
-	/*	//SLICO (no parameter to tune)
+		//SLICO (no parameter to tune)
 		start = System.currentTimeMillis();
-		Image outputSLICO = SLICO.exec(input, numberOfSuperPixels);
+		Image outputSLICO = SLICO.exec(input, numberOfSuperPixels,fuseSuperpixels);
 		end = System.currentTimeMillis();
 		double executionTimeSLICO = end - start;
 		Viewer2D.exec(DrawFrontiersOnImage.exec(input, FrontiersFromSegmentation.exec(outputSLICO)),"SLICO : "+numberOfSuperPixels+" superpixels");
-*/
 
-	/*	//ASLIC (no parameter to tune)
+
+		//ASLIC (no parameter to tune)
 		start = System.currentTimeMillis();
-		Image outputASLIC = ASLIC.exec(input, numberOfSuperPixels);
+		Image outputASLIC = ASLIC.exec(input, numberOfSuperPixels,true);
 		end = System.currentTimeMillis();
 		double executionTimeASLIC = end - start;
 		Viewer2D.exec(DrawFrontiersOnImage.exec(input, FrontiersFromSegmentation.exec(outputASLIC)),"ASLIC : "+numberOfSuperPixels+" superpixels");
-	*/	
-		
-		
-		
-		System.out.println("Execution time for SLIC = "+executionTimeSLICOrphanPixels+"ms");
 
-	//	System.out.println("Execution time for SLICO = "+executionTimeSLICO+"ms");
 
-	//	System.out.println("Execution time for ASLIC = "+executionTimeASLIC+"ms");
-		
-		System.out.println("SLIC, SLICO and ASLIC tests terminated...");
-		 
 
-		//Image outputSLICO = SLICOWithPostProcess.exec(input, numberOfSuperPixels);
-		//Viewer2D.exec(DrawFrontiersOnImage.exec(input, FrontiersFromSegmentation.exec(outputSLICO)),"SLICO with post-process step : "+numberOfSuperPixels+" superpixels");
+		System.out.println("Execution time for SLIC = "+executionTimeSLIC+"ms");
+
+		System.out.println("Execution time for SLICO = "+executionTimeSLICO+"ms");
+
+		System.out.println("Execution time for ASLIC = "+executionTimeASLIC+"ms");
+
+		System.out.println("SLICs tests terminated...");
+
+
 	}
 }
